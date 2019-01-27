4eb71057623307f2db0afd4acfcb0c71589cedfe
Catriona Ford <catriona.b.ford@gmail.com>
2019-01-27 20:41:37 +0000

    all functions in instrument_families converted to class syntax

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -1,21 +1,42 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const InstrumentFamilies = function (data) {
-  this.data = data;
-};
+// const InstrumentFamilies = function (data) {
+//   this.data = data;
+// };
 
-InstrumentFamilies.prototype.bindEvents = function () {
-  PubSub.publish('InstrumentFamilies:data-ready', this.data);
+class InstrumentFamilies {
+  constructor(data){
+    this.data = data;
+  }
 
-  PubSub.subscribe('SelectView:change', (evt) => {
-    const selectedIndex = evt.detail;
-    this.publishFamilyDetail(selectedIndex);
-  });
-};
+  bindEvents(){
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
 
-InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
-  const selectedFamily = this.data[selectedIndex];
-  PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
-};
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  }
+
+  publishFamilyDetail(selectedIndex){
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  }
+
+}
+
+// InstrumentFamilies.prototype.bindEvents = function () {
+//   PubSub.publish('InstrumentFamilies:data-ready', this.data);
+//
+//   PubSub.subscribe('SelectView:change', (evt) => {
+//     const selectedIndex = evt.detail;
+//     this.publishFamilyDetail(selectedIndex);
+//   });
+// };
+
+// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+//   const selectedFamily = this.data[selectedIndex];
+//   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+// };
 
 module.exports = InstrumentFamilies;
